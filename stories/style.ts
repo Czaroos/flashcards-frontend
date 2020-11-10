f754bb1e97e6c88a71df6698f9fb9876d10997c1
Czaroos <czaroosik@gmail.com>
2020-11-10 11:55:18 +0100

    added storybook, configured webpack, added jest and necessary dependencies

---
+++ stories/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+export const AbsoluteItemContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 800px;
+  width: 800px;
+  position: relative;
+`;
