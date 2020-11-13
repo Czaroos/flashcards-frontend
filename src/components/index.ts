012e37fb028902808d98742f7d9ea30a5863e18a
Czaroos <czaroosik@gmail.com>
2020-11-13 14:27:30 +0100

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,4 @@
 export * from "./button";
-<<<<<<< HEAD
 export * from "./modal";
-=======
 export * from "./icon-button";
->>>>>>> ec95aa204fb14f3136173153957802f7ec0a188b
 export * from "./navbar";
