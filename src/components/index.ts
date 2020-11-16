55bc5d694ea3667d5de9271453ba5cfd5a5b0723
Czaroos <35345462+Czaroos@users.noreply.github.com>
2020-11-16 10:53:50 +0100

    sign-up-modal-content (#14)
    * added modal component
    
    * added icon-button component. added google logo.
    
    * customized google button
    
    * fix
    
    * added sign in modal content. waiting for inputs
    
    * added sign up modal content

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./button";
 export * from "./modal";
 export * from "./icon-button";
+export * from "./sign-up";
 export * from "./sign-in";
 export * from "./navbar";
 export * from "./input";
---
+++ src/components/sign-up/style.ts
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+import { Button } from "@components";
+
+export const SignUpContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  width: 600px;
+  border: 1px solid var(--details);
+  background: var(--primary-light);
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  color: var(--white);
+  width: 100%;
+  padding: 32px;
+  background: var(--details);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 32px;
+`;
+
+export const Google = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border: 1px solid var(--secondary);
+  border-radius: 4px;
+  padding: 12px;
+  width: 100%;
+
+  & > h2 > span {
+    &:first-child {
+      color: #4285f4;
+    }
+
+    &:nth-child(2) {
+      color: #ea4335;
+    }
+
+    &:nth-child(3) {
+      color: #fbbc04;
+    }
+
+    &:nth-child(4) {
+      color: #4285f4;
+    }
+
+    &:nth-child(5) {
+      color: #34a853;
+    }
+
+    &:last-child {
+      color: #ea4335;
+    }
+  }
+`;
+
+export const Inputs = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 580px;
+  border: 1px solid var(--secondary);
+  border-radius: 4px;
+  padding: 16px;
+  margin: 24px 0;
+
+  > h3 {
+    color: var(--grey);
+  }
+`;
