77790d089bd3cf2cb9840ccef6c9065faa837764
AjronN <34678770+ajronn@users.noreply.github.com>
2020-11-19 11:15:38 +0100

    add colors for flashcard and fix some css

--- stories/Board.stories.tsx
+++ stories/Board.stories.tsx
@@ -3,9 +3,10 @@ import { storiesOf } from "@storybook/react";
 import { Board, Item } from "@components";
 
 const elements: Item[] = [
-    { id: "tiny_0", coords: { x: 0, y: 0 }, variant: "tiny" },
-    { id: "medium_0", coords: { x: 1, y: 1 }, variant: "medium" },
-    { id: "large_0", coords: { x: 0, y: 4 }, variant: "large" },
+    { id: "tiny_0", coords: { x: 0, y: 0 }, variant: "tiny", color: "pink" },
+    { id: "medium_0", coords: { x: 1, y: 1 }, variant: "medium", color: "gray" },
+    { id: "large_0", coords: { x: 0, y: 4 }, variant: "large", color: "blue" },
+    { id: "tiny_1", coords: { x: 2, y: 1 }, variant: "tiny" },
 ]
 
 storiesOf("Board", module)
