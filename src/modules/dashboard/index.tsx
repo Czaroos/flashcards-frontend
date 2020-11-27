2ce55854c077b8173c8dca6d59cdbe4774ed5622
i0skar <32570470+i0skar@users.noreply.github.com>
2020-11-27 12:26:04 +0100

    form modals for decs (#41)
    * form modals for decs
    
    * refactor v1
    
    * propagation fix
    
    Co-authored-by: Oskar <oskarbrudnowski@gmail.com>
    Co-authored-by: Czaroos <czaroosik@gmail.com>

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -3,17 +3,43 @@ import { useHistory } from "react-router-dom";
 
 import { useAuthProvider } from "@core/auth";
 
-import { Button } from "@components";
+import { Button, Modal } from "@components";
 
 import { Deck, getDecks, createDeck, deleteDeck, editDeck } from "@firebase";
-import { DashboardContainer } from "./style";
+import {
+  DashboardContainer,
+  ModalContainer,
+  MyBtn,
+  Separator,
+  Title,
+} from "./style";
 
 const Dashboard = () => {
   const [decks, setDecks] = useState<Deck[]>([]);
 
+  const [openAdd, setOpenAdd] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+
+  const [addInputValue, setAddInputValue] = useState("");
+  const [editInputValue, setEditInputValue] = useState("");
+
+  const [currentId, setCurrentId] = useState("");
+
   const { user } = useAuthProvider();
 
   const history = useHistory();
+
+  const handleNewDeck = (e: any) => {
+    e.preventDefault();
+    createDeck(addInputValue, user!.id);
+    setOpenAdd(false);
+  };
+
+  const handleEditDeck = (e: any) => {
+    e.preventDefault();
+    handleEdit(currentId, editInputValue);
+    setOpenEdit(false);
+  };
 
   const handleEdit = async (id: string, name: string) => {
     const error = await editDeck({ id, userId: user!.id, name });
@@ -45,33 +71,87 @@ const Dashboard = () => {
   }, [user]);
 
   return (
-    // CHANGE WHEN CATEGORY / DECK COMPONENT IS FINISHED
-    // THIS IS ONLY FOR TEST PURPOSES
-    // BUTTON ADDS A DECK
-    // CLICKING ON BUTTON NEXT TO DECK'S NAME DELETES IT
     <div>
-      <Button width="300px" onClick={() => createDeck("deck1", user!.id)}>
+      <Button width="300px" onClick={() => setOpenAdd(true)}>
         Create new deck
       </Button>
       {decks &&
         decks.map((deck, idx) => {
           return (
-            <DashboardContainer key={idx}>
-              <h2 onClick={() => history.push(`/decks/${deck.id}`)}>
-                {deck.name} - click me to go to my dashboard - edited at -
-                {deck.editedAt}- edited by - {deck.editedBy}
-              </h2>
-              <Button
-                onClick={() => {
-                  handleEdit(deck.id, "new Name");
-                }}
+            <>
+              <DashboardContainer
+                key={idx}
+                onClick={() => history.push(`/decks/${deck.id}`)}
               >
-                EDIT
-              </Button>
-              <Button onClick={() => deleteDeck(deck.id)}>DELETE</Button>
-            </DashboardContainer>
+                <h2>
+                  {deck.name} - click me to go to my dashboard - edited at -
+                  {deck.editedAt}- edited by - {deck.editedBy}
+                </h2>
+                <Button
+                  onClick={() => {
+                    setOpenEdit(true);
+                    setCurrentId(deck.id);
+                    setEditInputValue(deck.name);
+                  }}
+                >
+                  EDIT
+                </Button>
+                <Button onClick={() => deleteDeck(deck.id)}>DELETE</Button>
+              </DashboardContainer>
+            </>
           );
         })}
+      <Modal open={openAdd} setOpen={setOpenAdd}>
+        <ModalContainer>
+          <Title>
+            <div>Add</div>
+            <img
+              onClick={() => setOpenAdd(false)}
+              src="https://www.flaticon.com/svg/static/icons/svg/447/447047.svg"
+              alt="close"
+              height="16"
+              width="16"
+            />
+          </Title>
+          <Separator />
+          <form onSubmit={handleNewDeck}>
+            <label>Name</label>
+            <input
+              type="text"
+              value={addInputValue}
+              onChange={(e: any) => setAddInputValue(e.target.value)}
+            />
+            <Separator />
+            <MyBtn onClick={handleNewDeck}>Confirm</MyBtn>
+          </form>
+        </ModalContainer>
+      </Modal>
+
+      <Modal open={openEdit} setOpen={setOpenEdit}>
+        <ModalContainer>
+          <Title>
+            <div>Edit</div>
+            <img
+              onClick={() => setOpenEdit(false)}
+              src="https://www.flaticon.com/svg/static/icons/svg/447/447047.svg"
+              alt="close"
+              height="16"
+              width="16"
+            />
+          </Title>
+          <Separator />
+          <form onSubmit={handleEditDeck}>
+            <label>Name</label>
+            <input
+              type="text"
+              value={editInputValue}
+              onChange={(e: any) => setEditInputValue(e.target.value)}
+            />
+            <Separator />
+            <MyBtn onClick={handleEditDeck}>Confirm</MyBtn>
+          </form>
+        </ModalContainer>
+      </Modal>
     </div>
   );
 };
--- src/modules/dashboard/style.ts
+++ src/modules/dashboard/style.ts
@@ -1,3 +1,4 @@
+import { Button } from "@components";
 import styled from "styled-components";
 
 export const DashboardContainer = styled.div`
@@ -9,8 +10,74 @@ export const DashboardContainer = styled.div`
   margin: 10px 50px;
   cursor: pointer;
 
-  h2 {
+  > h2 {
     font-size: 22px;
     margin: 10px 20px;
   }
+`;
+
+export const ModalContainer = styled.div`
+  background: white;
+  width: 300px;
+  margin: 50px auto;
+  float: none;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid rgba(0,0,0,.2);
+  border-radius: .3rem;
+
+  > form {
+    display: flex;
+    flex-direction: column;
+
+    > label {
+      margin-top: 20px;
+      margin-bottom: 10px;
+      margin-left: 20px;
+      margin-right: 20px;
+    }
+
+    > input {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+      justify-content: space-around;
+      width: auto;
+      border: 1px solid var(--secondary);
+      border-radius: 4px;
+      padding: 16px;
+      margin: 0px 20px;
+      margin-bottom: 20px;
+      font-size: 18px;
+    }
+  }
+`;
+
+export const MyBtn = styled(Button)`
+  margin: 20px;
+`;
+
+export const Title = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 20px;
+
+    > img {
+      cursor: pointer;
+
+      :hover {
+        filter: drop-shadow(2px 4px 6px black);
+      }
+    }
+
+    > div {
+      font-weight: bold;
+      font-size: 20px;
+    }
+`;
+
+export const Separator = styled.div`
+  border-bottom: 1px solid #80808075;
+  margin: 0px 20px;
 `;
