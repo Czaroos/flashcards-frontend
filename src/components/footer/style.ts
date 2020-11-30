f9b1b7241378a2b5f21c65b87ebde6e836dd76f4
Oskar <oskarbrudnowski@gmail.com>
2020-11-30 09:57:55 +0100

    footer rebuild

--- src/components/footer/style.ts
+++ src/components/footer/style.ts
@@ -1,3 +1,4 @@
+import { Button } from "@components";
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
@@ -5,7 +6,7 @@ export const FooterContainer = styled.div`
   align-items: center;
   grid-template-columns: 1fr max(1200px) 1fr;
   width: 100%;
-  padding: 150px 0;
+  padding: 120px 0;
   color: var(--white);
   background: linear-gradient(10deg,var(--primary-light),var(--secondary-light));
 
@@ -20,21 +21,34 @@ export const FooterContainer = styled.div`
   section {
     grid-column: 2/3;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-around;
 
     div {
       display: flex;
       flex-direction: column;
 
-      p {
-        font-weight: 1000;
-        font-size: 17px;
-        margin: 10px 0px;
+      h1 {
+        letter-spacing: 3px;
+        padding: 10px;
       }
 
       a {
         cursor: pointer;
+        text-indent: 20px;
+        font-size: 20px;
+        margin: 5px;
       }
     }
+  }
+`;
+
+export const StyledButton = styled(Button)`
+  border: 2px solid var(--white);
+  width: 200px;
+  margin: 5px 20px;
+  cursor: pointer;
+
+  &:hover{
+    opacity: 0.8;
   }
 `;
