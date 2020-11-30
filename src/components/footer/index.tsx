70d961e90fd3fb1599ab0f0832fd40382b86b614
i0skar <32570470+i0skar@users.noreply.github.com>
2020-11-30 11:45:26 +0100

    footer rebuild (#46)
    * footer rebuild
    
    * change v1
    
    Co-authored-by: Oskar <oskarbrudnowski@gmail.com>

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,33 +1,29 @@
 import React from "react";
-import { FooterContainer } from "./style";
+import { FooterContainer, StyledButton } from "./style";
 
 export const Footer = () => {
   return (
     <FooterContainer>
       <section>
         <div>
-          <p>Kategoria</p>
-          <a>Lorem</a>
-          <a>Ipsum dolor</a>
-          <a>Sit amet</a>
-          <a>Consectetur</a>
-          <a>Adipiscing elit</a>
+          <h1>Navigation</h1>
+          <a>Home</a>
+          <a>About</a>
+          <a>Log in</a>
+          <a>Sign up</a>
         </div>
         <div>
-          <p>Kategoria</p>
-          <a>Lorem</a>
-          <a>Ipsum dolor</a>
-          <a>Sit amet</a>
-          <a>Consectetur</a>
-          <a>Adipiscing elit</a>
+          <h1>Useful links</h1>
+          <a>Services</a>
+          <a>Support</a>
+          <a>Terms &amp; Conditions</a>
+          <a>Privacy Policy</a>
         </div>
         <div>
-          <p>Kategoria</p>
-          <a>Lorem</a>
-          <a>Ipsum dolor</a>
-          <a>Sit amet</a>
-          <a>Consectetur</a>
-          <a>Adipiscing elit</a>
+          <h1>Subscribe to Newsletter</h1>
+          <StyledButton variant="transparent">Subscribe</StyledButton>
+          <h1>Contact us</h1>
+          <a>flashyourcard@gmail.com</a>
         </div>
       </section>
     </FooterContainer>
--- src/components/footer/style.ts
+++ src/components/footer/style.ts
@@ -1,3 +1,4 @@
+import { Button } from "@components";
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
@@ -5,9 +6,10 @@ export const FooterContainer = styled.div`
   align-items: center;
   grid-template-columns: 1fr max(1200px) 1fr;
   width: 100%;
-  padding: 150px 0;
+  padding: 120px 0;
   color: var(--white);
   background: linear-gradient(10deg,var(--primary-light),var(--secondary-light));
+  font-size: 0.9em;
 
   @media (max-width: 1250px) {
     grid-template-columns: 1fr 30fr 1fr;
@@ -20,21 +22,34 @@ export const FooterContainer = styled.div`
   section {
     grid-column: 2/3;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-around;
 
     div {
       display: flex;
       flex-direction: column;
 
-      p {
-        font-weight: 1000;
-        font-size: 17px;
-        margin: 10px 0px;
+      h1 {
+        letter-spacing: 3px;
+        padding: 10px;
       }
 
       a {
         cursor: pointer;
+        text-indent: 20px;
+        font-size: 18px;
+        margin: 5px;
       }
     }
+  }
+`;
+
+export const StyledButton = styled(Button)`
+  border: 2px solid var(--white);
+  width: 200px;
+  margin: 5px 20px;
+  cursor: pointer;
+
+  &:hover{
+    opacity: 0.8;
   }
 `;
